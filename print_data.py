ba3a1f15630993a05009312bb504222ed7ea7e70
zeynep cahan <144728809+zypchn@users.noreply.github.com>
2025-12-02 19:57:13 +0300

    Update print_data.py

--- print_data.py
+++ print_data.py
@@ -66,15 +66,15 @@ def getMatrix(dirname):
     print(reMatrix.shape)
     return reMatrix
 
-x1_ds2 = getMatrix("data/T4SE_train1502/result/negative/pssm_profile_uniref50")
-x2_ds2 = getMatrix("data/T4SE_train1502/result/positive/pssm_profile_uniref50")
+x1 = getMatrix("data/T4SE_train1502/result/negative/pssm_profile_uniref50/pssm_profile_uniref50")
+x2 = getMatrix("data/T4SE_train1502/result/positive/pssm_profile_uniref50/pssm_profile_uniref50")
 y = [-1 for i in range(x1.shape[0])]
 y.extend([1 for i in range(x2.shape[0])])
 y = np.array(y)
 x = np.vstack((x1, x2))
 
-test_x1 = getMatrix("data/T4SE_test180/result/negative/pssm_profile_uniref50")
-test_x2 = getMatrix("data/T4SE_test180/result/positive/pssm_profile_uniref50")
+test_x1 = getMatrix("data/T4SE_test180/result/negative/pssm_profile_uniref50/pssm_profile_uniref50")
+test_x2 = getMatrix("data/T4SE_test180/result/positive/pssm_profile_uniref50/pssm_profile_uniref50")
 
 test_x = np.vstack((test_x1, test_x2))
 test_y = [-1 for i in range(test_x1.shape[0])]
@@ -88,4 +88,5 @@ test_x = x_all[1502:, :]
 print(x)
 print()
 print(y)
-print("ddd")
+print()
+print(test_x)
